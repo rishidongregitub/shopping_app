a1dace001be46da69df98e6c74face4f31edcbad
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-06-30 20:28:45 +0530

    Navbar_footer

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
   return (
     <div>
         <Header/>
-        <main style={{minHeight :'80vh'}}>{children}</main>
+        <main style={{minHeight :'67.4vh'}}>{children}</main>
         <Footer/>
     </div>
   )
