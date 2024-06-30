7bf9cfe3c30dc3d51f2b775dc22455a2fb2b54f3
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-06-30 21:41:08 +0530

    Contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
-
+import Layout from "./../components/Layout/Layout";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 const Contact = () => {
   return (
-    <Layout>
-      <h1>Contact</h1>
+    <Layout title={"Contact us"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
+          <p className="text-justify mt-2">
+            Any query and info about Product feel free to call anytime we 24X7
+            available
+          </p>
+          <p className="mt-3">
+            <BiMailSend /> : rishidongre519.com
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : +91 8517800519
+          </p>
+          <p className="mt-3">
+            <BiSupport /> : 1800-0000-0000 (toll free)
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
