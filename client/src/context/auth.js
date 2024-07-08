3bc4511925aaa6670709451c177760b73b29887b
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-07-08 20:13:57 +0530

    contextapi setup

---
+++ client/src/context/auth.js
@@ -0,0 +1,19 @@
+import { useState, useContext, createContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+  });
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+//custom Hook
+const useAuth = () => useContext(AuthContext);
+export { useAuth, AuthProvider };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-
+import { useAuth } from '../context/auth'
 const HomePage = () => {
+  const [auth,setAuth] = useAuth();
   return (
     <Layout title={"Best Offer - Shop Now"}>
         <h1>HomePage</h1>
+        <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
 
   )
