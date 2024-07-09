99074621b0bfbe607f5d52fa49a448cbe36048e9
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-07-09 09:17:07 +0530

    fixes of dashboard route for logged user and add spinner

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const Spinner = () => {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+        setCount((prevValue)=>--prevValue)
+    },1000)
+    count === 0 && navigate('/login')
+    return ()=>clearInterval(interval)
+  },[count,navigate])
+  return (
+    <>
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <h1 className="Text-center">redirecting to you in {count} second</h1>
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -16,5 +16,9 @@ router.post('/login',loginController)
 //test routes
 router.get('/test', requireSignIn, isAdmin, testController)
 
+//protected route
+router.get("/user-auth", requireSignIn, (req,res)=>{
+    res.status(200).send({ok:true})
+})
 
 export default router;
