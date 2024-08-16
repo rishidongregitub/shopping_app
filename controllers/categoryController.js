99e88f9da89502ca32734331f413f088cfba903f
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-08-16 17:14:07 +0530

    create category with admin authorization

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,5 @@
 import slugify from "slugify";
-import categoryModel from "../models/categoryModel";
+import categoryModel from "../models/categoryModel.js";
 
 export const createCategoryController = async (req, res) => {
     try {
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,14 @@
-import express from 'express'
-import { createCategoryController } from '../controllers/categoryController'
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware'
+import express from "express";
+import { createCategoryController } from "../controllers/categoryController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
 
-const router = express.Router()
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
 
-router.post('create-category', requireSignIn,isAdmin, createCategoryController)
-
-export default router
+export default router;
